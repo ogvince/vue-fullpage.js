7cf142e53751ecab1c67ee50c7c425ee3b1d7535
ogvince <38331051+ogvince@users.noreply.github.com>
2018-04-13 01:03:27 +0200

    Create Constants.js

---
+++ demos/demo1/src/Constants.js
@@ -0,0 +1,8 @@
+export const GET_HOME_PROJECTS_API = 'wp/v2/projects?auteur=6';
+export const GET_USER_API = 'wp/v2/auteur';
+export const BASE_API = 'http://neueweb.studio/nema/wp-json/';
+export const GET_AUTHOR_PROJECTS_API = 'wp/v2/projects?auteur=';
+
+export const GET_PHOTOGRAPERS_API = 'wp/v2/photographe';
+export const GET_PHOTOGRAPER_PROJECTS_API = 'wp/v2/photo?photographe=';
+export const GET_CONTACT_API = 'wp/v2/contact';
